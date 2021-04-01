21fd29a201450791ecd913dd8fa4b245ae13cb54
Егор <egor_ars2000@mail.ru>
2021-04-01 19:08:04 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
+class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
+}
+class MainClass {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+        plate.info();
+        cat.eat(plate);
+        plate.info();
+    }
+}
+
